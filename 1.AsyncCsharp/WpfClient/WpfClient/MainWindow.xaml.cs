4ef111030f2f233f8c794d2c4ee3ec3f5f3063d3
Hachem Chrayti <hachem.chrayti@outlook.fr>
2021-03-18 10:18:06 +0100

    projet WCF initial(en mode sychrone)

---
+++ 1.AsyncCsharp/WpfClient/WpfClient/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+
+namespace WpfClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            progress.IsIndeterminate = true;
+            progress.Visibility = Visibility.Visible;
+
+            var client = new WebClient();
+
+            var response = client.DownloadString("http://localhost:9876/WeatherForecast");
+
+            var forecasts = JsonConvert.DeserializeObject<List<WeatherForecast>>(response);
+
+            tb_result.Text = string.Join(Environment.NewLine, forecasts.Select(f => $"Le {f.Date:dd/MM/yyyy}, il fera {f.TemperatureC}°"));
+
+            progress.IsIndeterminate = false;
+            progress.Visibility = Visibility.Hidden;
+        }
+    }
+}
